fa1a7eead78071fce0b5b95b3022c240a6f82a40
ILLIA <kraxrelax@gmail.com>
2025-04-15 18:17:29 +0200

    Adaptation sections

--- js/formats-slider.js
+++ js/formats-slider.js
@@ -1,16 +1,35 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-container', {
   // Optional parameters
-
   loop: true,
+
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 768px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.carousel-button.next-slide',
+    prevEl: '.carousel-button.prev-slide',
   },
 });
